0369fbc5156b2a8761a051c346e55db0f1d3832c
chuancw <2402873932@qq.com>
2018-07-26 21:26:13 +0800

    comparable

---
+++ src/main/java/com/wangchuan/lambda/Person.java
@@ -0,0 +1,26 @@
+package com.wangchuan.lambda;
+
+public class Person implements Comparable {
+
+    private int age;
+
+
+    /**
+     * -1  小到大
+     * 1   大到小
+     * @param o
+     * @return
+     */
+    public int compareTo(Object o) {
+        Person person = (Person)o;
+        return -Double.compare(this.age,person.age);
+    }
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/main/java/com/wangchuan/lambda/TestLambda.java
@@ -0,0 +1,21 @@
+package com.wangchuan.lambda;
+
+import java.util.Arrays;
+
+public class TestLambda {
+
+    public static void main(String[] args) {
+
+
+        Person person1 = new Person(23);
+        Person person2 = new Person(24);
+
+        System.out.println(person1.compareTo(person2));
+
+        Person[] person = {person1, person2};
+        Arrays.sort(person);
+        for(Person persons:person){
+            System.out.println(persons.getAge());
+        }
+    }
+}
